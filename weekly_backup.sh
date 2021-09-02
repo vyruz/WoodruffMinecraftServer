218092460f0075a99cadd41eba0d7a23643da409
luke <lwvyruz@gmail.com>
2021-09-02 16:10:51 -0600

    weekly backup

--- weekly_backup.sh
+++ weekly_backup.sh
@@ -1,7 +1,6 @@
-# Using this via cron job, such as
-# * 4 * * 0 /home/luke/WoodruffMinecraftServer/weekly_backup.sh
+#!/bin/bash
 
-cp -r /opt/minecraft/survival /home/luke/WoodruffMinecraftServer/
-git add .
-git commit -m "weekly backup"
-git push -u origin master
+cp -r /opt/minecraft/survival /home/luke/WoodruffMinecraftServer/ ;
+git add . ;
+git commit -m "weekly backup" ;
+git push -u origin master ;
