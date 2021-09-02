80ea723e1895a4195a90029e8eb1469a58ea5e29
luke <lwvyruz@gmail.com>
2021-09-02 17:44:21 -0600

    weekly backup

--- weekly_backup.sh
+++ weekly_backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+echo "test test test start" >> /home/luke/crontest
 cd /home/luke/WoodruffMinecraftServer/
 cp -r /opt/minecraft/survival /home/luke/WoodruffMinecraftServer/ ;
 git add . ;
 git commit -m "weekly backup" ;
 git push -u origin master ;
-echo "test test test" >> /home/luke/crontest
+echo "test test test end" >> /home/luke/crontest
