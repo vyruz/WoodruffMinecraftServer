0ffd92c8036671787a773000eec9d0546303d298
luke <lwvyruz@gmail.com>
2021-09-02 18:07:22 -0600

    weekly backup

--- weekly_backup.sh
+++ weekly_backup.sh
@@ -4,4 +4,5 @@ cp -r /opt/minecraft/survival /home/luke/WoodruffMinecraftServer/ ;
 /usr/bin/git add . ;
 /usr/bin/git commit -m "weekly backup" ;
 #/usr/bin/git push origin master ;
+/usr/bin/git remote add origin git@github.com:vyruz/WoodruffMinecraftServer.git
 /usr/bin/git push git@github.com:vyruz/WoodruffMinecraftServer.git master
