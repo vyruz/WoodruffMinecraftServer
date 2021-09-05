1ecb9ea45e438092002e70ff2565b6683b7689df
luke <lwvyruz@gmail.com>
2021-09-05 04:00:24 -0600

    weekly backup

--- weekly_backup.sh
+++ weekly_backup.sh
@@ -3,11 +3,9 @@
 eval $(ssh-agent)
 ssh-add /home/luke/.ssh/vyruz-git
 
-cd /home/luke/WoodruffMinecraftServer/;
-cp -r /opt/minecraft/survival /home/luke/WoodruffMinecraftServer/ ;
-/usr/bin/git add . ;
-/usr/bin/git commit -m "weekly backup" ;
-#/usr/bin/git push origin master ;
-/usr/bin/git remote set-url origin git@github.com:vyruz/WoodruffMinecraftServer.git
+cd /home/luke/WoodruffMinecraftServer/
+cp -r /opt/minecraft/survival /home/luke/WoodruffMinecraftServer/
+
+/usr/bin/git add .
+/usr/bin/git commit -m "weekly backup"
 /usr/bin/git push git@github.com:vyruz/WoodruffMinecraftServer.git master
-/usr/bin/git remote -v >> /home/luke/WoodruffMinecraftServer/remote
